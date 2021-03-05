600d85477c71b2375618727332d512cb4971b036
sformichella <sformichella@gmail.com>
2021-03-05 13:48:04 -0800

    add easy and hard tests

--- __tests__/arithmetic-addition.test.js
+++ __tests__/arithmetic-addition.test.js
@@ -2,6 +2,33 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('/arithmetic/addition routes', () => {
+  it('returns some easy addition problems', async() => {
+    const { body: problems } = await request(app)
+      .get('/api/v1/arithmetic/addition?number=5&difficulty=easy');
+
+    expect(problems.length).toBe(5);
+
+    const { 
+      leftNumber, 
+      rightNumber, 
+      equation, 
+      solution,
+      operation
+    } = problems[0];
+
+    expect(leftNumber).toEqual(expect.any(Number));
+    expect(String(leftNumber).length).toBe(1);
+
+    expect(rightNumber).toEqual(expect.any(Number));
+    expect(String(rightNumber).length).toBe(1);
+
+    expect(equation).toEqual(expect.any(String));
+
+    expect(solution).toEqual(expect.any(Number));
+
+    expect(operation).toEqual({ text: 'addition', symbol: '+' });
+  });
+
   it('returns some medium addition problems', async() => {
     const { body: problems } = await request(app)
       .get('/api/v1/arithmetic/addition?number=5&difficulty=medium');
@@ -21,6 +48,33 @@ describe('/arithmetic/addition routes', () => {
 
     expect(rightNumber).toEqual(expect.any(Number));
     expect(String(rightNumber).length).toBe(2);
+
+    expect(equation).toEqual(expect.any(String));
+
+    expect(solution).toEqual(expect.any(Number));
+
+    expect(operation).toEqual({ text: 'addition', symbol: '+' });
+  });
+
+  it('returns some hard addition problems', async() => {
+    const { body: problems } = await request(app)
+      .get('/api/v1/arithmetic/addition?number=5&difficulty=hard');
+
+    expect(problems.length).toBe(5);
+
+    const { 
+      leftNumber, 
+      rightNumber, 
+      equation, 
+      solution,
+      operation
+    } = problems[0];
+
+    expect(leftNumber).toEqual(expect.any(Number));
+    expect(String(leftNumber).length).toBe(3);
+
+    expect(rightNumber).toEqual(expect.any(Number));
+    expect(String(rightNumber).length).toBe(3);
 
     expect(equation).toEqual(expect.any(String));
 
