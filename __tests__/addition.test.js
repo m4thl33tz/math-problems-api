209b6c0b3cb51cb41c49744dbfcbe342fd4ddd3a
sformichella <sformichella@gmail.com>
2021-03-05 13:23:50 -0800

    test scaffolding

---
+++ __tests__/addition.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('math-problems-api routes', () => {
+  it('returns some addition problems', () => {
+    
+  });
+});
