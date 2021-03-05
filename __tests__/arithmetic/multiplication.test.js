1fc6458d750126c047dbc73440ba971e8f4b8b9b
sformichella <sformichella@gmail.com>
2021-03-05 15:14:07 -0800

    add tests for multiplication

---
+++ __tests__/arithmetic/multiplication.test.js
@@ -0,0 +1,85 @@
+const request = require('supertest');
+const app = require('../../lib/app');
+
+describe('/arithmetic/multiplication routes', () => {
+  it('returns some easy multiplication problems', async() => {
+    const { body: problems } = await request(app)
+      .get('/api/v1/arithmetic/multiplication?number=5&difficulty=easy');
+
+    expect(problems.length).toBe(5);
+
+    const { 
+      leftNumber, 
+      rightNumber, 
+      equation, 
+      solution,
+      operation
+    } = problems[0];
+
+    expect(leftNumber).toEqual(expect.any(Number));
+    expect(String(leftNumber).length).toBe(1);
+
+    expect(rightNumber).toEqual(expect.any(Number));
+    expect(String(rightNumber).length).toBe(1);
+
+    expect(equation).toEqual(expect.any(String));
+
+    expect(solution).toEqual(expect.any(Number));
+
+    expect(operation).toEqual({ text: 'multiplication', symbol: '*' });
+  });
+
+  it('returns some medium multiplication problems', async() => {
+    const { body: problems } = await request(app)
+      .get('/api/v1/arithmetic/multiplication?number=5&difficulty=medium');
+
+    expect(problems.length).toBe(5);
+
+    const { 
+      leftNumber, 
+      rightNumber, 
+      equation, 
+      solution,
+      operation
+    } = problems[0];
+
+    expect(leftNumber).toEqual(expect.any(Number));
+    expect(String(leftNumber).length).toBe(2);
+
+    expect(rightNumber).toEqual(expect.any(Number));
+    expect(String(rightNumber).length).toBe(2);
+
+    expect(equation).toEqual(expect.any(String));
+
+    expect(solution).toEqual(expect.any(Number));
+
+    expect(operation).toEqual({ text: 'multiplication', symbol: '*' });
+  });
+
+  it('returns some hard multiplication problems', async() => {
+    const { body: problems } = await request(app)
+      .get('/api/v1/arithmetic/multiplication?number=5&difficulty=hard');
+
+    expect(problems.length).toBe(5);
+
+    const { 
+      leftNumber, 
+      rightNumber, 
+      equation, 
+      solution,
+      operation
+    } = problems[0];
+
+    expect(leftNumber).toEqual(expect.any(Number));
+    expect(String(leftNumber).length).toBe(3);
+
+    expect(rightNumber).toEqual(expect.any(Number));
+    expect(String(rightNumber).length).toBe(3);
+
+    expect(equation).toEqual(expect.any(String));
+
+    expect(solution).toEqual(expect.any(Number));
+
+    expect(operation).toEqual({ text: 'multiplication', symbol: '*' });
+  });
+});
